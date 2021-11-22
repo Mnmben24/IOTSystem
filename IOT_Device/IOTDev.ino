5f41f96a79d059e167f9bdc398b97c495874904b
Marco Benvenuti <marcobenvenuti96@gmail.com>
2021-11-22 20:42:42 +0200

    IOT Device Connects to Server

--- IOT_Device/IOTDev.ino
+++ IOT_Device/IOTDev.ino
@@ -4,59 +4,77 @@
 #include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager
 
 
-#define SendKey 0
+#define Status_Pin 4
+#define Relay_Pin 5
 
 int port = 8123;
 String host = "192.168.68.118";
+
+
+int device_status = 0;
 
 WiFiManager wifiManager;
 // Use WiFiClient class to create TCP connections
 WiFiClient client;
 
+void ConnectToServer(){
+  Serial.println("Connecting to Server");
+  while (!client.connect(host, port)){
+    Serial.println("connection failed");
+    delay(5000);
+  }
+  Serial.println("Connected to Server");
+  client.println("{'Name': 'Garage', 'Type': 'OnOffStatus'}");
+}
+
+void CloseServerConn(){
+  // Close the connection
+  Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+}
+
+void Trigger_Relay() {
+  digitalWrite(Relay_Pin, HIGH);
+  delay(100);
+  digitalWrite(Relay_Pin, LOW);
+}
+
 void setup() {
   Serial.begin(115200);
+
+  pinMode(Status_Pin, INPUT);
+  pinMode(Relay_Pin, OUTPUT);
+
+  digitalWrite(Relay_Pin, LOW);
   
   WiFiServer server(port);
-
-  
   
   // Uncomment and run it once, if you want to erase all the stored information
   // wifiManager.resetSettings();
 
   wifiManager.autoConnect("SmartHome");
   
-  Serial.println("Connected.");
+  Serial.println("Connected to Network");
 
-  while (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    delay(5000);
-  }
+  ConnectToServer();
+  
 }
 
 void loop(){
-
-  
-
-  // This will send a string to the server
-  Serial.println("sending data to server");
-  if (client.connected()) {
-    client.println("{'Name': 'Garage', 'Type': 'OnOffStatus'}");
-  }
-  else {
-    Serial.println("Connecting to the Server");
-    if (client.connect(host, port)){
-      Serial.println("Connection Failed");
-      delay(5000);
+  device_status = digitalRead(Status_Pin);
+    Serial.println("Status: " + String(device_status));
+  if (client.connected()){
+    Serial.println("Sending...");
+    client.print(device_status);
+    while (!client.available())  {delay(10);}
+    String line = client.readStringUntil('\n');
+    Serial.println("Recieved " + line);
+    if (line == "1") {
+      Trigger_Relay();
     }
   }
-
-  while (client.available() == 0) {}
-  char ch = static_cast<char>(client.read());
-  Serial.print(ch);
-
-  // Close the connection
-  Serial.println();
-  Serial.println("closing connection");
-  client.stop();
-
+  else{
+    ConnectToServer();
+  }
 }
